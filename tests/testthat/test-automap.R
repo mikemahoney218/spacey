d989d16401beae0d6d7ecfd6e8fd8edff274b95a
Mike Mahoney <mike.mahoney.218@gmail.com>
2020-02-20 19:39:48 -0500

    Hopefully fix appveyor

--- tests/testthat/test-automap.R
+++ tests/testthat/test-automap.R
@@ -24,15 +24,7 @@ test_that("automap creates the same map twice", {
 
 test_that("automap builds from local files", {
   skip_on_cran()
-  boston_map <- tempfile("boston", fileext = ".png")
-  x <- automap(lat = 42.3601, lng = -71.0589, z = 0.3)
-  rayshader::save_png(x, boston_map)
-  expect_equal(
-    load_overlay(boston_map),
-    load_overlay(pkgload::package_file("vignettes/boston_simple.png")),
-    tolerance = 0.004
-  )
-  expect_invisible(automap(
+    expect_invisible(automap(
     from.file = TRUE,
     tif.filename = "../data/boston_heights.tif",
     png.filename = "../data/boston_overlay.png",
@@ -68,6 +60,19 @@ test_that("automap warnings fire appropriately", {
       "#F7E29E"
     ))
   ))
+})
+
+test_that("automap reproduces maps", {
+  skip_on_cran()
+  skip_on_appveyor() # appveyor dislikes this pathing
+  boston_map <- tempfile("boston", fileext = ".png")
+  x <- automap(lat = 42.3601, lng = -71.0589, z = 0.3)
+  rayshader::save_png(x, boston_map)
+  expect_equal(
+    load_overlay(boston_map),
+    load_overlay(pkgload::package_file("vignettes/boston_simple.png")),
+    tolerance = 0.004
+  )
 })
 
 test_that("automap errors fire appropriately", {
